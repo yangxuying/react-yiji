a9d98825c01dcfd38025e24550b11c1a979ccddb
yangxuying <2710343126@qq.com>
2019-08-29 10:58:37 +0800

    my

---
+++ precache-manifest.06e749fc5e196637989e9f56562adbcb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215b02e0efdca807b3ca73c3a6104e15",
+    "url": "/react-yiji/index.html"
+  },
+  {
+    "revision": "7c197c7214568c6b4a03",
+    "url": "/react-yiji/static/css/2.fb164c73.chunk.css"
+  },
+  {
+    "revision": "6f7db1ab201351d07c6c",
+    "url": "/react-yiji/static/css/main.e9b36a05.chunk.css"
+  },
+  {
+    "revision": "7c197c7214568c6b4a03",
+    "url": "/react-yiji/static/js/2.e77690e6.chunk.js"
+  },
+  {
+    "revision": "6f7db1ab201351d07c6c",
+    "url": "/react-yiji/static/js/main.c5e1bca5.chunk.js"
+  },
+  {
+    "revision": "76e3e25619ead56721a2",
+    "url": "/react-yiji/static/js/runtime~main.df17a495.js"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/react-yiji/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/react-yiji/static/media/element-icons.d2f69a92.woff"
+  }
+]);
--- src/store/actionTypes.js
+++
@@ -1,4 +0,0 @@
-// export const GET_PRODUCTS = 'GET_PRODUCTS';
-// export const ADD_TOCART = 'ADD_TOCART';
-export const ADD_COMMENT = 'ADD_COMMENT';
-export const Like = 'Like';
